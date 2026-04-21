fdadce73835a34f1b4c1362833e5b7321c209951
frank7841 <franklinewax@gmail.com>
2026-04-21 14:12:50 +0300

    deploy to render

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "www.figma.com",
         pathname: "/api/mcp/asset/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
       },
     ],
   },
